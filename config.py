2782aab9a30287742ddfc731655b469312553dba
eoghanlochlyneoghanlochlyn <eoghanlochlyn@atomicmail.io>
2026-09-25 19:06:28 +0330

    BNJ: add report configuration

---
+++ config.py
@@ -0,0 +1,21 @@
+from zoneinfo import ZoneInfo
+
+IRAN_TIMEZONE = ZoneInfo("Asia/Tehran")
+
+PRIORITY_TEAMS = {
+    "Liverpool", "Arsenal", "Manchester City", "Manchester United",
+    "Chelsea", "Tottenham Hotspur", "Juventus", "AC Milan", "Inter",
+    "Bayern Munich", "Borussia Dortmund", "Paris Saint-Germain",
+    "Real Madrid", "Barcelona", "Atlético Madrid",
+}
+
+MAJOR_LEAGUES = {"Premier League", "LaLiga", "Serie A", "Bundesliga", "Ligue 1"}
+
+COMPETITION_KEYWORDS = {
+    "uefa champions league", "uefa europa league", "uefa conference league",
+    "fa cup", "efl cup", "carabao cup", "copa del rey", "coppa italia",
+    "dfb-pokal", "coupe de france", "super cup", "uefa super cup",
+    "afc champions league elite", "afc champions league two",
+    "uefa nations league", "european championship", "world cup",
+    "asian cup", "copa america", "club world cup",
+}
